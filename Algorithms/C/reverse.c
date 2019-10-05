7f12d0d622ccd440508e21b88495d0a73cbd64db
jasondavindev <jasondavin.dev@gmail.com>
2019-10-05 14:51:40 -0300

    implemented reverse array in C

---
+++ Algorithms/C/reverse.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void reverse(int *, int, int);
+void swap(int *, int *);
+void print_array(int *, int);
+
+int main()
+{
+  int array[] = {1, 2, 3, 4, 5, 6, 7, 8};
+  reverse(array, 0, 8 - 1); // last index
+  print_array(array, 8);
+  return 0;
+}
+
+/**
+ * swap array values
+ */
+void swap(int *i, int *j)
+{
+  int temp = *i;
+  *i = *j;
+  *j = temp;
+}
+
+/**
+ * recursive reverse function
+ * takes array's reference, first and last index of array
+ */
+void reverse(int *array, int begin, int end)
+{
+  /**
+   * greater than or equal because depends of number of elements
+   * if even, must be greater than
+   * if odd, must be equals
+   */
+  if (begin >= end)
+  {
+    return;
+  }
+
+  swap(&array[begin], &array[end]);
+  reverse(array, ++begin, --end);
+}
+
+void print_array(int *array, int length)
+{
+  for (int i = 0; i < length; i++)
+  {
+    printf("%d ", array[i]);
+  }
+
+  printf("\n");
+}
